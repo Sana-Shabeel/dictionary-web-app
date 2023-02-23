8d264abdc8b572a6fdb4be9e09ba4480e00ccd90
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-23 22:47:49 +0300

    Add navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Image,
+  Switch,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import DropDown from "./DropDown";
+import MoonIcon from "./MoonIcon";
 
 const Navbar = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  // const color = useColorModeValue("gray", "blue");
+
   return (
     <Flex
-      justifyContent={"space-between"}
+      justifyContent="space-between"
       alignItems="center"
-      width="100vw"
-      border={"1px solid"}
-      padding="0 2em"
+      width="100%"
+      padding="2em 0"
+      marginInline="auto"
     >
-      <Flex fontSize="3rem">Logo</Flex>
-      <DropDown />
+      <Image src="/public/images/logo.svg" />
+      <Flex alignItems="center" gap={{ base: "8px", md: "1rem" }}>
+        <DropDown />
+        <Box height="2rem" width="1.6px" bg={"lightGray"}></Box>
+        <Flex alignItems="center" gap={{ base: "10px", md: "1rem" }}>
+          <Switch
+            size={{ base: "sm", md: "lg" }}
+            onChange={() => toggleColorMode()}
+          />
+          <MoonIcon />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
