7a47c218f81ec8e87ddf51ee84b0e2fcae05dad7
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-23 06:41:32 +0300

    Add custom chakra theme

---
+++ src/chakra/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    input: "#f4f4f4",
+    lightGray: "#e9e9e9",
+    darkGray: "#757575",
+    purple: "#a445ed",
+    lightBlack: "#2d2d2d",
+    darkerBlack: "#1F1F1F",
+    veryBlack: "#050505",
+    lineGray: "#3a3a3a",
+  },
+});
