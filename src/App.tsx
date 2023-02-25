a0370c2e991bd969d2c502b0da7b4a7d9ce3b043
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-25 15:09:32 +0300

    Add useQuery functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,55 @@ import {
 import axios from "axios";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import DisplayData from "./components/displayData/DisplayData";
 import InputField from "./components/Input/Input";
 import Navbar from "./components/Navbar/Navbar";
+import useDebounce from "./hooks/useDebounce";
+
+const fetchData = (value: string | undefined) => {
+  return axios
+    .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`)
+    .then((res) => res.data);
+};
 
 function App() {
+  const [value, setValue] = useState("keyboard");
+  const debouncedValue = useDebounce(value, 200);
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["inputValue"],
+    queryFn: () => {
+      console.log("fetching");
+
+      return fetchData(debouncedValue);
+    },
+  });
+
+  const onSubmitValue = (
+    e: React.FormEvent,
+    inputRef: React.RefObject<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    inputRef.current?.blur();
+
+    refetch();
+  };
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <pre>error</pre>;
+
+  console.log(data[0].word);
+
   return (
     <Box width={{ base: "93%", lg: "800px" }} marginInline="auto">
       <Navbar />
-      <InputField />
+      <InputField
+        value={value}
+        setValue={setValue}
+        onSubmitValue={onSubmitValue}
+      />
 
-      <Text fontSize="5rem" textAlign="center">
-        hello
-      </Text>
+      <DisplayData />
     </Box>
   );
 }
