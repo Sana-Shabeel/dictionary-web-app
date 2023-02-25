231e99033de1affe639d32f6d3e0d7d765738106
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-25 15:10:09 +0300

    Add display data component

---
+++ src/components/displayData/DisplayData.tsx
@@ -0,0 +1,16 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const DisplayData = () => {
+  return (
+    <Box>
+      <Flex>
+        <Text fontSize="3rem" fontWeight="bold">
+          Keybaord
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default DisplayData;
