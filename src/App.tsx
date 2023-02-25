56130c1691d24b16acd463990d236af7dd8b80c0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-25 14:29:22 +0300

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,21 @@ import {
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { useState } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import InputField from "./components/Input/Input";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const { toggleColorMode, colorMode } = useColorMode();
-  const textColor = useColorModeValue("lightGray", "purple");
-
-  console.log(colorMode);
-
   return (
     <Box width={{ base: "93%", lg: "800px" }} marginInline="auto">
       <Navbar />
       <InputField />
+
+      <Text fontSize="5rem" textAlign="center">
+        hello
+      </Text>
     </Box>
   );
 }
