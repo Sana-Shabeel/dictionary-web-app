de95798e906af753a88f3a13b88a44182e807d7c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-25 14:30:18 +0300

    Add custom hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { delay } from "framer-motion";
+import { useState, useEffect } from "react";
+
+// pass the input value as the value to this function
+export default function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
