977121ed746a393690b9895fde4dedf8c4308ab9
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-25 14:29:38 +0300

    Add react query

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,23 +1,53 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { InputGroup, InputRightElement, Input } from "@chakra-ui/input";
 import { Button, useColorModeValue } from "@chakra-ui/react";
-import React, { useState, useRef } from "react";
+import axios from "axios";
+import React, { useState, useRef, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import useDebounce from "../../hooks/useDebounce";
+
+interface Props {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  // onSubmitValue: (e: React.FormEvent) => void;
+}
+
+const fetchData = (value: string) => {
+  return axios
+    .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${value}`)
+    .then((res) => res.data);
+};
 
 const InputField = () => {
-  const [value, setValue] = useState("");
+  const queryClient = useQueryClient();
   const bg = useColorModeValue("input", "darkerBlack");
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const [value, setValue] = useState("keyboard");
+
+  // const debouncedInputValue = useDebounce(value, 200);
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["inputValue"],
+    queryFn: () => fetchData(value),
+  });
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <pre>error</pre>;
+
   const onSubmitValue = (e: React.FormEvent) => {
     e.preventDefault();
-
     inputRef.current?.blur();
-    console.log(value);
+    refetch();
   };
+
+  console.log(data[0].word);
+
   return (
     <form onSubmit={onSubmitValue}>
       <InputGroup size="md">
         <Input
+          defaultValue={value}
           ref={inputRef}
           pr="4.5rem"
           variant="filled"
