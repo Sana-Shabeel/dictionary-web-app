408864da666a7dcc3a8363be0ed3f639538d1f18
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-26 09:02:29 +0300

    Add meaning compn

---
+++ src/components/displayData/MeaningList.tsx
@@ -0,0 +1,65 @@
+import {
+  Divider,
+  Flex,
+  ListItem,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { Meaning, Root, Root2 } from "../../model";
+
+interface Props {
+  meaning: Meaning;
+}
+
+const MeaningList = ({ meaning }: Props) => {
+  // console.log("inside Meaning comp", meaning);
+
+  return (
+    <Box margin={"1rem 0"}>
+      <Flex alignItems="center" mt={5}>
+        <Text fontWeight="bold">{meaning.partOfSpeech}</Text>
+        <Divider ml="1rem" />
+      </Flex>
+
+      <Text
+        margin="1em 0"
+        color="gray"
+        fontSize={{ base: "1rem", md: "1.2rem" }}
+      >
+        Meaning
+      </Text>
+      <UnorderedList>
+        {meaning.definitions.map(({ definition, example }) => (
+          <>
+            <ListItem fontSize={{ base: "1rem", md: "1.2rem" }}>
+              {definition}
+            </ListItem>
+            <Text color="gray" fontSize={{ base: "1rem", md: "1.2rem" }}>
+              {example ? `"${example}"` : null}
+            </Text>
+          </>
+        ))}
+      </UnorderedList>
+
+      {/* Synonyms */}
+      <Flex alignItems="center" gap={2} maxWidth="100vw" flexWrap="wrap">
+        <Text
+          maxWidth="100vw"
+          color="gray"
+          fontSize={{ base: "1rem", md: "1.2rem" }}
+          mr="1rem"
+        >
+          Synonyms
+        </Text>
+
+        {meaning.synonyms.map((synonym) => (
+          <span className="span-synonyms">{synonym}</span>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default MeaningList;
