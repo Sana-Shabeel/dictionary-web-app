2d81abf07e20b47badb6de1727c1109825e86005
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-26 06:01:00 +0300

    Add Icons svg

---
+++ src/icons/MoonIcon.tsx
@@ -0,0 +1,29 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+
+const MoonIcon = () => {
+  const color = useColorModeValue("gray", "blue");
+
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="22"
+        height="22"
+        style={{ color: color }}
+        viewBox="0 0 22 22"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default MoonIcon;
